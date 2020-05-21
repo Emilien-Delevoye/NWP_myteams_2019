fe1f187596373bc64c6dc8ee08b8a0bf65cba51e
Emilien Delevoye <emilien.delevoye@epitech.eu>
2020-05-21 12:23:43 +0200

    Update server command to not login other user login event

--- client/src/server_commands/login.c
+++ client/src/server_commands/login.c
@@ -10,10 +10,12 @@
 
 void login_server(char read_buf[BF_S], struct client_s *cli)
 {
-    if (strncmp("login", read_buf, 5) != 0)
-        return;
-    memcpy(cli->user.uuid, read_buf + 6, sizeof(cli->user.uuid));
-    memcpy(cli->user.username, read_buf + 7 + sizeof(cli->user.uuid),
-        sizeof(cli->user.username));
-    client_event_loggedin((const char *)cli->user.uuid, cli->user.username);
+    if (strncmp("login", read_buf, 5) == 0) {
+        memcpy(cli->user.uuid, read_buf + 6, sizeof(cli->user.uuid));
+        memcpy(cli->user.username, read_buf + 7 + sizeof(cli->user.uuid),
+            sizeof(cli->user.username));
+        client_event_loggedin(cli->user.uuid, cli->user.username);
+    } else if (strncmp("bogin", read_buf, 5) == 0) {
+        client_event_loggedin(cli->user.uuid, cli->user.username);
+    }
 }
