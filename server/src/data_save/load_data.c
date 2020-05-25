a0473abe4a10c78bd0fd4fd1e65c35894a93cba5
Emilien Delevoye <emilien.delevoye@epitech.eu>
2020-05-25 12:26:05 +0200

    Add user load at the server start

--- server/src/data_save/load_data.c
+++ server/src/data_save/load_data.c
@@ -7,6 +7,8 @@
 
 #include "server.h"
 #include <fcntl.h>
+#include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
 const char value_load[10] = "1234567890";
@@ -26,6 +28,26 @@ static char get_ctrl(int fd, char *ctrl)
     return (*ctrl);
 }
 
+void add_user_data(data_server_t *data, struct l_save_user_s cur)
+{
+    struct user_s *new = malloc(sizeof(struct user_s));
+    struct user_s *cur_list = data->l_users;
+
+    if (!new)
+        return;
+    memcpy(new->username, cur.user.username, sizeof(new->username));
+    memcpy(new->uuid, cur.user.uuid, sizeof(new->uuid));
+    new->next = NULL;
+    new->joined_teams = NULL;
+    if (!data->l_users) {
+        data->l_users = new;
+    } else {
+        while (cur_list->next)
+            cur_list = cur_list->next;
+        cur_list->next = new;
+    }
+}
+
 void load_data(data_server_t *data)
 {
     int fd = open("domain.save", O_RDONLY);
@@ -40,4 +62,6 @@ void load_data(data_server_t *data)
                 fct[a](fd, &load_data);
     for (struct l_save_team_s *cur = load_data.team; cur; cur = cur->next)
         add_team_data(data, *cur);
+    for (struct l_save_user_s *cur = load_data.user; cur; cur = cur->next)
+        add_user_data(data, *cur);
 }
