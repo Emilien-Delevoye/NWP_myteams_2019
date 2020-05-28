e2f40118ac9a5f179b37de30edf412e6ef9f529e
Emilien Delevoye <emilien.delevoye@epitech.eu>
2020-05-28 17:29:18 +0200

    Remove useless includes and remove unused warning in messages/messages.c

--- server/src/control_socket/accept_connections.c
+++ server/src/control_socket/accept_connections.c
@@ -10,10 +10,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <errno.h>
-#include <stdio.h>
 #include <netinet/in.h>
-#include <arpa/inet.h>
 
 static void init_client(struct client_s *n_client)
 {
