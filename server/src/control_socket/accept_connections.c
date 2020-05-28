90cde177060c0a9fbec340f8b982993a455f9cc2
Emilien Delevoye <emilien.delevoye@epitech.eu>
2020-05-28 16:51:30 +0200

    Events are not send to clients which are not logged

--- server/src/control_socket/accept_connections.c
+++ server/src/control_socket/accept_connections.c
@@ -29,22 +29,6 @@ static void init_client(struct client_s *n_client)
     n_client->to_delete = false;
 }
 
-static void send_already_logged_in(struct client_s *client,
-    data_server_t *data)
-{
-    struct packet_server_s packet = {0};
-
-    for (struct client_s *cur = data->l_clients; cur; cur = cur->next) {
-        if (!cur->user)
-            continue;
-        memset(&packet, 0, sizeof(packet));
-        memcpy(packet.user_id, cur->user->uuid, sizeof(packet.user_id));
-        memcpy(packet.name, cur->user->username, sizeof(packet.name));
-        packet.command = 1;
-        add_to_buffer_list(client, packet);
-    }
-}
-
 static void add_new_client(data_server_t *data, int new_fd)
 {
     struct client_s *n_client = malloc(sizeof(struct client_s));
@@ -63,7 +47,6 @@ static void add_new_client(data_server_t *data, int new_fd)
             current = current->next;
         current->next = n_client;
     }
-    send_already_logged_in(n_client, data);
 }
 
 void accept_connections(data_server_t *data)
@@ -75,8 +58,7 @@ void accept_connections(data_server_t *data)
     if (!FD_ISSET(data->control_sckt, &data->sckt_r))
         return;
     new_fd = accept(data->control_sckt, NEWADDR, LENADDR);
-    if (new_fd < 0) {
+    if (new_fd < 0)
         return;
-    }
     add_new_client(data, new_fd);
 }
